ca02c7f89156c05904d7d36099acafb2ab4271bb
João Victor <joao.adm.etec@outlook.com>
2024-03-11 01:15:56 -0300

    Aplicando migrations

---
+++ AppUnipsico/Migrations/20240311041505_Config datas disponiveis.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AppUnipsico.Migrations
+{
+    /// <inheritdoc />
+    public partial class Configdatasdisponiveis : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "DatasDisponiveis",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Data = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DatasDisponiveis", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DatasDisponiveis");
+        }
+    }
+}
