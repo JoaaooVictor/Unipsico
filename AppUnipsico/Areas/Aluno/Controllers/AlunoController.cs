ab90ea155723f7c40130fa8b265bfca82d5cfa3a
João Victor <joao.adm.etec@outlook.com>
2024-04-01 23:35:18 -0300

    Ajuste dos botões de detalhes e gerar pdf do aluno.

--- AppUnipsico/Areas/Aluno/Controllers/AlunoController.cs
+++ AppUnipsico/Areas/Aluno/Controllers/AlunoController.cs
@@ -3,6 +3,8 @@ using AppUnipsico.Areas.Admin.ViewModels;
 using AppUnipsico.Areas.Aluno.ViewModels;
 using AppUnipsico.Enums;
 using AppUnipsico.Models;
+using iText.Html2pdf;
+using iText.Kernel.Pdf;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -93,6 +95,40 @@ namespace AppUnipsico.Areas.Aluno.Controllers
 
             return View(estagioViewModel);
         }
+
+        public async Task<IActionResult> Detalhes(Guid estagioId)
+        {
+            if (string.IsNullOrEmpty(estagioId.ToString()))
+            {
+                return NotFound();
+            }
+
+            var estagio = await _estagioRepository.BuscaEstagioPorId(estagioId);
+
+            return View(estagio);
+        }
+
+        public async Task<IActionResult> GerarPdfEstagio(Guid estagioId)
+        {
+            var estagio = await _estagioRepository.BuscaEstagioPorId(estagioId);
+
+            if (estagio == null)
+            {
+                return NotFound();
+            }
+
+            var htmlContent = await _estagioRepository.RenderizarHtmlEstagio(estagio);
+
+            var memoryStream = new MemoryStream();
+            var writer = new PdfWriter(memoryStream);
+            var pdf = new PdfDocument(writer);
+            var converter = new ConverterProperties();
+            HtmlConverter.ConvertToPdf(htmlContent, pdf, converter);
+            pdf.Close();
+
+            return File(memoryStream.ToArray(), "application/pdf", Path.Combine($"{estagio.Aluno.RA}", "_estagio.pdf"));
+        }
+
         public IActionResult Presencas()
         {
             return View();
